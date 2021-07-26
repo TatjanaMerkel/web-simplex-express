a81d5ffb09c18dee2296dcdfd806b838e2a29732
Tatjana Merkel <tatjana_merkel@outlook.com>
2021-07-26 23:38:43 +0200

    Implement POST route

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
+const bodyParser = require('body-parser')
 const cors = require('cors')
 const express = require('express')
+const pg = require('pg')
 
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
 
 app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Working')
+})
+
+//
+// DATABASE SETUP
+//
+
+const pool = new pg.Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'web_simplex_db',
+    password: '1234',
+    port: 5432,
 })
 
 //
@@ -17,7 +38,37 @@ app.get('/', (req, res) => {
 app.post('/exercise', (req, res) => {
     console.log(`${req.method} ${req.url}`)
 
-    res.status(201).send('CREATE')
+    const sqlParams = [
+        req.body.title,
+        req.body.difficulty,
+        req.body.task,
+        parseInt(req.body.numberOfVars),
+        parseInt(req.body.numberOfConstraints),
+        JSON.stringify(JSON.parse(req.body.targetVars)),
+        JSON.stringify(JSON.parse(req.body.constraintVars)),
+        JSON.stringify(JSON.parse(req.body.constraintVals))
+    ]
+
+    const createSql = `
+        INSERT INTO exercises (title,
+                               difficulty,
+                               task,
+                               number_of_vars,
+                               number_of_constraints,
+                               target_vars,
+                               constraint_vars,
+                               constraint_vals)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+        RETURNING *
+    `
+
+    pool.query(createSql, sqlParams, (error, result) => {
+        if (error) {
+            throw error
+        }
+
+        res.status(201).json(result.rows[0])
+    })
 })
 
 //
